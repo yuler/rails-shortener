a36f9194b4a6a55e5633faa2a770bf4f2da5e988
yuler <is.yuler@gmail.com>
2023-11-22 17:27:22 +0800

    Add gavatar_image_tag helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def gavatar_image_tag(email, **kwarg)
+    image_tag gavatar_url(email), **kwarg
+  end
+
+  # refs: https://docs.gravatar.com/gravatar-images/ruby/
+  def gavatar_url(email)
+    hash = Digest::MD5.hexdigest(email)
+    return "https://www.gravatar.com/avatar/#{hash}"
+  end
 end
